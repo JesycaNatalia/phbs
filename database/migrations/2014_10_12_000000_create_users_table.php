b879b4c0a34070f415b2abb3f9e87109d60cb798
alfydape <daffareri6@gmail.com>
2022-06-12 13:36:35 +0800

    user fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('nama');
+            $table->string('no_kk');
+            $table->string('jenis_kelamin');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
